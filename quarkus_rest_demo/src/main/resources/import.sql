efb119018a71de08fc5899eb2389eb084c6c94cb
jdeshmukh <jaydeep.268@gmail.com>
2020-02-24 11:35:00 -0500

    project name renamed with quarkus_rest_demo

---
+++ quarkus_rest_demo/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO order_table(id, itemName, orderBy, orderDate) VALUES (nextval('hibernate_sequence'), 'TV','01-Jan-2019');
+INSERT INTO order_table(id, itemName, orderBy, orderDate) VALUES (nextval('hibernate_sequence'), 'Cricket Bat','17-Nov-2019');
+INSERT INTO order_table(id, itemName, orderBy, orderDate) VALUES (nextval('hibernate_sequence'), 'Iphone-X','27-Jan-2020');
